6f533e909a2a1023f8e00f4236bee5abd1303dbb
luis cavalcante <luis@turbot.com>
2022-09-28 18:19:49 -0300

    Jenkins Job table

--- jenkins/plugin.go
+++ jenkins/plugin.go
@@ -19,7 +19,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			Schema:      ConfigSchema,
 		},
 		TableMap: map[string]*plugin.Table{
-			// "jenkins_job": tableJenkinsJob(),
+			"jenkins_job": tableJenkinsJob(),
 		},
 	}
 
