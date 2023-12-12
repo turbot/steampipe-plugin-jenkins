08f37cb57b213d41c7a0c5ba5ac62494b1c81724
Ved misra <47312748+misraved@users.noreply.github.com>
2023-12-12 21:10:24 +0530

    Recompile plugin with steampipe-plugin-sdk v5.8.0 (#19)
    Co-authored-by: Madhushree Ray <madhushree@turbot.com>

--- jenkins/connection_config.go
+++ jenkins/connection_config.go
@@ -2,25 +2,12 @@ package jenkins
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type jenkinsConfig struct {
-	ServerURL *string `cty:"server_url"`
-	Username  *string `cty:"username"`
-	Password  *string `cty:"password"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"server_url": {
-		Type: schema.TypeString,
-	},
-	"username": {
-		Type: schema.TypeString,
-	},
-	"password": {
-		Type: schema.TypeString,
-	},
+	ServerURL *string `hcl:"server_url"`
+	Username  *string `hcl:"username"`
+	Password  *string `hcl:"password"`
 }
 
 func ConfigInstance() interface{} {
